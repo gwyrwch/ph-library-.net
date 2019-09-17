a50b6207038e5ad8a2482c5cc02a285fb8700e4f
gwyrwch <gwyrwch@gmail.com>
2019-09-17 13:45:49 +0300

    added minimal working registration of the user, without checking and saving password  and login view

--- ph/Controllers/AuthController.cs
+++ ph/Controllers/AuthController.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ph.Models;
 
 namespace ph.Controllers
 {
@@ -7,12 +13,46 @@ namespace ph.Controllers
         // GET
         public IActionResult Index()
         {
-            return Redirect("Auth/Registration");
+            return Redirect("Auth/Login");
         }
 
-        public IActionResult Registration()
+        public async Task<IActionResult> Registration()
         {
             return View();
+        }
+
+        [HttpPost]
+        // todo: add checking password
+        public async Task<IActionResult> Registration([Bind("UserName, Email")]User user)
+        {
+            Console.WriteLine("kek");
+            var alreadyExists = TmpRAMDB.Users().Count(u => u.Email == user.Email || u.UserName == user.UserName);
+            if (alreadyExists != 0)
+            {
+                return Redirect("/Home/Feed");
+            }
+            
+
+            if (user.Email != string.Empty && user.UserName != String.Empty)
+            {
+                user.Id = (uint) (user.UserName + user.Email).GetHashCode();
+                TmpRAMDB.Users().Add(user);
+                return Redirect("/Home/Feed");
+            }
+
+            return Redirect("/Home/Feed");
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        // todo: add checking password
+        public async Task<IActionResult> Login([Bind("UserName")] User user)
+        {
+            return Redirect("/Home/Feed");
         }
     }
 }
