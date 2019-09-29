3ff0b7093a6067fcf7425f8932e722a71e4e8e0f
gwyrwch <gwyrwch@gmail.com>
2019-09-29 17:05:21 +0300

    first changes to createUser form

--- ph/wwwroot/js/site.js
+++ ph/wwwroot/js/site.js
@@ -2,17 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
-
-Element.prototype.remove = function() {
-    this.parentElement.removeChild(this);
-};
-
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
-};
