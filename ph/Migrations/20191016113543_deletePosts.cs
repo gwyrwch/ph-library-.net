7cb08de9fb4fa1486bf8b319c1456343df72ef8c
gwyrwch <gwyrwch@gmail.com>
2019-10-16 17:18:19 +0300

    everything is broken 2.0

---
+++ ph/Migrations/20191016113543_deletePosts.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ph.Migrations
+{
+    public partial class deletePosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IncludedPetId",
+                table: "Posts");
+
+            migrationBuilder.RenameTable(
+                name: "Posts",
+                newName: "Post");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_UserId",
+                table: "Post",
+                newName: "IX_Post_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Post",
+                nullable: false,
+                oldClrType: typeof(uint))
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PetId",
+                table: "Post",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Post",
+                table: "Post",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_AspNetUsers_UserId",
+                table: "Post",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_AspNetUsers_UserId",
+                table: "Post");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Post",
+                table: "Post");
+
+            migrationBuilder.DropColumn(
+                name: "PetId",
+                table: "Post");
+
+            migrationBuilder.RenameTable(
+                name: "Post",
+                newName: "Posts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Post_UserId",
+                table: "Posts",
+                newName: "IX_Posts_UserId");
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "Id",
+                table: "Posts",
+                nullable: false,
+                oldClrType: typeof(string))
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "IncludedPetId",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
